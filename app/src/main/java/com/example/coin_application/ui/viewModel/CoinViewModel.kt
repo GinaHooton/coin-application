a738ebe5527b53dbd20cec90e9a4e5e0eae8786d
ghooton2 <gghooton@gmail.com>
2025-03-28 15:13:31 +0000

    added template files and hilt dependency

---
+++ app/src/main/java/com/example/coin_application/ui/viewModel/CoinViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.coin_application.ui.viewModel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class CoinViewModel {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "coin-application"
 include(":app")
-include(":app")
 include(":data")
 include(":domain")
